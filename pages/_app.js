050e2417af9b1893f02c94962fc8c289b167ae88
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-11-10 15:59:09 +0700

    set up mdb

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../components/style.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
