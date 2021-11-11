86bde33a983fe7662fc6ca7b8a1903a51646eff4
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-11-11 17:04:02 +0700

    'nice-icon-love'

--- components/Box.jsx
+++ components/Box.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Card from "react-bootstrap/Card";
-import { Button } from "react-bootstrap"
 import { Row } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col'
 import { Container } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import { AwesomeButton } from "react-awesome-button";
 
 export default function Box() {
     const [data, setdata] = useState([])
@@ -24,13 +23,22 @@ export default function Box() {
                 <Row >
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[0].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[0].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>1. {data.data?.top[0].title} </MDBCardTitle>
                                 <MDBCardText>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </MDBCardText>
-                                <MDBBtn href='#'>Button</MDBBtn>
+
+                                
+                                <AwesomeButton
+                                    size="icon"
+                                    href="https://img.icons8.com/color/48/000000/youtube-play.png"
+                                    type="primary"
+                                    
+                                >
+                                    <img src="https://www.img.in.th/images/cf9793aaac2dcb9d12d5cefdf7452b1f.png" alt="love" width="30rem" />
+                                </AwesomeButton>
                             </MDBCardBody>
                         </MDBCard>
 
@@ -38,7 +46,7 @@ export default function Box() {
 
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[1].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[1].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>2. {data.data?.top[1].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -51,7 +59,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[2].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[2].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>3. {data.data?.top[2].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -66,9 +74,9 @@ export default function Box() {
 
                 </Row>
                 <Row style={{ paddingTop: 60 }}>
-                <Col classname='boxs' md={6} lg={4}>
+                    <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[3].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[3].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>4. {data.data?.top[3].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -81,7 +89,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[4].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[4].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>5. {data.data?.top[4].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -94,7 +102,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[5].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[5].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>6. {data.data?.top[5].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -117,9 +125,9 @@ export default function Box() {
 
                 </Row>
                 <Row style={{ paddingTop: 60 }} classname='boxs'>
-                <Col classname='boxs' md={6} lg={4}>
+                    <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[6].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[6].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>7. {data.data?.top[6].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -132,7 +140,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[7].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[7].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>8. {data.data?.top[7].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -145,7 +153,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[8].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[8].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>9. {data.data?.top[8].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -167,9 +175,9 @@ export default function Box() {
 
                 </Row>
                 <Row style={{ paddingTop: 60 }}>
-                <Col classname='boxs' md={6} lg={4}>
+                    <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[9].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[9].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>10. {data.data?.top[9].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -182,7 +190,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[10].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[10].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>11. {data.data?.top[10].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -195,7 +203,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[11].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[11].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>12. {data.data?.top[11].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -218,9 +226,9 @@ export default function Box() {
 
                 </Row>
                 <Row style={{ paddingTop: 60 }}>
-                <Col classname='boxs' md={6} lg={4}>
+                    <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[12].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[12].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>13. {data.data?.top[12].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -233,7 +241,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[13].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[13].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>14. {data.data?.top[13].title} </MDBCardTitle>
                                 <MDBCardText>
@@ -246,7 +254,7 @@ export default function Box() {
                     </Col>
                     <Col classname='boxs' md={6} lg={4}>
                         <MDBCard style={{ maxWidth: '18rem' }}>
-                            <MDBCardImage src={data.data?.top[14].image_url}  style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
+                            <MDBCardImage src={data.data?.top[14].image_url} style={{ width: '18rem', height: '18rem' }} position='top' alt='...' />
                             <MDBCardBody>
                                 <MDBCardTitle>15. {data.data?.top[14].title} </MDBCardTitle>
                                 <MDBCardText>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../components/style.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import "react-awesome-button/dist/styles.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
