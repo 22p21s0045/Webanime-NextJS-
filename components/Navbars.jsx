6c8da61a231e756059eb385cb9df9e48285e1125
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2021-12-11 21:41:08 +0700

    'log-in-success'

--- components/Navbars.jsx
+++ components/Navbars.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import {Navbar, Nav, NavItem,Container} from 'react-bootstrap';
 import { MDBBtn } from 'mdb-react-ui-kit';
+import { authen } from './Store/firebase';
+import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 export default function Navbars() {
+  const provider = new GoogleAuthProvider();
+  const signIn = () => {
+    signInWithPopup(authen, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
     return (
         <div>
             
@@ -22,7 +45,7 @@ export default function Navbars() {
     </Container>
     <Nav className="navbar-right">
       <div style = {{paddingRight:50}}>
-      <MDBBtn float = 'right'style = {{}}>LOGIN</MDBBtn>
+      <MDBBtn float = 'right' onClick = {signIn}>LOGIN</MDBBtn>
       </div>
 
       </Nav>
